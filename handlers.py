3c75a024176f447268ec322b28fc793ee069a4ea
Simeon Mugisha <cmeo226@yahoo.com>
2014-07-22 21:18:02 +0800

    Initial commit.

---
+++ handlers.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+import subprocess
+
+def handle_msg(msg):
+    return
+    
+#plays the media files
+def media_player(media):
+    subprocess.call(["/usr/bin/omxplayer", media])
+
+#turns lights on or off
+def toggle_on_off_lights():
+    return
+
+
