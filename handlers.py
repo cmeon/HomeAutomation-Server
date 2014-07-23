f85f7aa4b7675b1e5291bc82c7a84f9c87ef14a9
Simeon Mugisha <cmeo226@yahoo.com>
2014-07-23 23:49:14 +0800

    added AsyncTask class for threaded processes

--- handlers.py
+++ handlers.py
@@ -1,16 +1,31 @@
 #!/usr/bin/python
 
 import subprocess
+import threading, zipfile
 
 def handle_msg(msg):
     return
     
 #plays the media files
 def media_player(media):
-    subprocess.call(["/usr/bin/omxplayer", media])
+    background = AsyncTask('/usr/bin/vlc', media)
+    background.setDaemon(True)
+    background.start()
+    print('The main program continues to run in foreground.')
 
 #turns lights on or off
 def toggle_on_off_lights():
-    return
+    background = AsyncTask('/usr/bin/totem', media)
+    background.start()
+    print('The main program continues to run in foreground.')
 
-
+# This class performs playing the player in the background and
+# brings it to the foreground when it is already playing
+class AsyncTask(threading.Thread):
+    def __init__(self, player, media_file):
+        threading.Thread.__init__(self)
+        self.player = player
+        self.media_file = media_file
+    def run(self):
+        subprocess.call([self.player, self.media_file])
+        print('done with bg process')
--- name.py
+++ name.py
@@ -11,13 +11,14 @@ options = {
 }
 
 movie = {
-   "break": "/home/pi/break.avi"
+   "break": '/home/cmeon/Downloads/Veep.S03E06.HDTV.x264-2HD.mp4'
 }
 
+a = 1
 def message_handler(conn_object, message_node):
    print message_node.getBody()
-#   command = message_node.getBody().split()
-#   options[command[0]](movie[command[1]])
+   command = message_node.getBody().split()
+   options[command[0]](movie[command[1]])
 
 def presence_handler(conn_object, presence_node):
    print presence_node.getAttr("from")
@@ -25,7 +26,7 @@ def presence_handler(conn_object, presence_node):
 
 jid=xmpp.JID(user)
 connection = xmpp.Client(jid.getDomain())
-connection.connect(server=("talk.google.com", 5223))
+connection.connect()
 print "done connection"
 result=connection.auth(jid.getNode(), password, "Some Client-Name")
 
